ffc2f28b960ab72eaf913d8804e7655558ae96a8
yor_email <daniil.stebnitsky@gmail.com>
2021-09-28 21:43:48 +0300

    ForwardLinked: added function addFirst(T value)

--- src/test/java/ru/job4j/chapter1/list/linked/ForwardLinkedTest.java
+++ src/test/java/ru/job4j/chapter1/list/linked/ForwardLinkedTest.java
@@ -33,4 +33,16 @@ public class ForwardLinkedTest {
         Iterator<Integer> it = linked.iterator();
         assertThat(it.next(), is(2));
     }
+
+    @Test
+    public void whenAddFirst() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.add(1);
+        linked.add(2);
+        linked.addFirst(0);
+        Iterator<Integer> it = linked.iterator();
+        assertThat(it.next(), is(0));
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(2));
+    }
 }
