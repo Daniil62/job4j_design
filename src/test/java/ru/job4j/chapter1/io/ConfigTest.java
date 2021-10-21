f6699196e293778a21c501c359ab61aa2d2a1e10
yor_email <daniil.stebnitsky@gmail.com>
2021-10-21 16:42:38 +0300

    1. Читаем файл конфигурации [#858]

---
+++ src/test/java/ru/job4j/chapter1/io/ConfigTest.java
@@ -0,0 +1,51 @@
+package ru.job4j.chapter1.io;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "./data/app.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.dialect"), is("org.hibernate.dialect.PostgreSQLDialect"));
+        assertThat(config.value("###"), is(Matchers.nullValue()));
+        assertThat(config.size(), is(5));
+    }
+
+    @Test
+    public void whenPairHasOnlyComments() {
+        String path = "./data/comments.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.size(), is(0));
+    }
+
+    @Test
+    public void whenPairHasCommentAndEmptyLines() {
+        String path = "./data/with_comment_and_empty_lines.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.size(), is(3));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenPairInvalidate() {
+        String path = "./data/invalidate.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+
+    @Test
+    public void whenPairIsEmpty() {
+        String path = "./data/empty.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.size(), is(0));
+    }
+}
