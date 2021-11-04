800868c121d04346f20260fef78c77e92805673e
yor_email <daniil.stebnitsky@gmail.com>
2021-11-04 21:37:17 +0300

    1. Бот [#7921]

--- src/main/java/ru/job4j/chapter1/socket/EchoSocked.java
+++ src/main/java/ru/job4j/chapter1/socket/EchoSocked.java
@@ -9,22 +9,48 @@ import java.net.Socket;
 
 public class EchoSocked {
 
-    public static void main(String[] args) throws IOException {
+    public void connection() throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write(MessageStore.RESPONSE_SUCCESSFUL_CONNECTION_MSG.getBytes());
+                    StringBuilder builder = new StringBuilder();
                     for (String line = in.readLine(); line != null && !line.isEmpty(); line = in.readLine()) {
-                        System.out.println(line);
-                        if (line.contains("?msg=Bye")) {
+                        if (line.contains(MessageStore.REQUEST_EXIT_SEQUENCE)) {
                             server.close();
+                            break;
                         }
+                        builder.append(line);
                     }
+                    sendResponse(out, builder.toString());
                     out.flush();
                 }
             }
         }
+    }
+
+    private void sendResponse(OutputStream out, String request) throws IOException {
+        if (request != null && !request.isEmpty()) {
+            if (request.contains(MessageStore.REQUEST_GREETING_SEQUENCE)) {
+                out.write(MessageStore.RESPONSE_GREETING_MSG.getBytes());
+            } else {
+                out.write(MessageStore.RESPONSE_DEFAULT_MSG.getBytes());
+            }
+        }
+    }
+
+    private static class MessageStore {
+
+        private static final String RESPONSE_SUCCESSFUL_CONNECTION_MSG = "HTTP/1.1 200 OK\r\n\r\n";
+        private static final String RESPONSE_GREETING_MSG = "Hello, dear friend.\r\n\r\n";
+        private static final String RESPONSE_DEFAULT_MSG = "What? \r\n\r\n";
+        private static final String REQUEST_GREETING_SEQUENCE = "?msg=Hello";
+        private static final String REQUEST_EXIT_SEQUENCE = "?msg=Exit";
+    }
+
+    public static void main(String[] args) throws IOException {
+       new EchoSocked().connection();
     }
 }
