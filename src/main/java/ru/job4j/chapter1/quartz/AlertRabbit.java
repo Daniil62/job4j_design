fb10dc7849a305e649218b08784ebbf97a8db05f
yor_email <daniil.stebnitsky@gmail.com>
2021-12-08 14:28:38 +0300

    1.1. Job c параметрами [#260360]

--- src/main/java/ru/job4j/chapter1/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/chapter1/quartz/AlertRabbit.java
@@ -4,6 +4,8 @@ import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.InputStream;
+import java.sql.*;
+import java.time.LocalDateTime;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.newJob;
@@ -13,16 +15,18 @@ import static org.quartz.TriggerBuilder.newTrigger;
 public class AlertRabbit {
 
     private final static String PATH = "rabbit.properties";
-    private final static String PERIOD_KEY = "rabbit.interval";
+    private Properties properties;
     private int period;
+    private final JobDataMap data = new JobDataMap();
 
     private void init() {
         try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream(PATH)) {
-            Properties properties = new Properties();
+            properties = new Properties();
             if (in != null) {
                 properties.load(in);
             }
-            period = Integer.parseInt(properties.getProperty(PERIOD_KEY));
+            Class.forName(properties.getProperty(Keys.DRIVER));
+            period = Integer.parseInt(properties.getProperty(Keys.PERIOD));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -30,28 +34,73 @@ public class AlertRabbit {
 
     public void run() {
         init();
-        try {
+        try (Connection connection = DriverManager.getConnection(
+                properties.getProperty(Keys.URL),
+                properties.getProperty(Keys.USER),
+                properties.getProperty(Keys.PASSWORD)
+        )) {
+            data.put(Keys.JOB_DATA_MAP_CONTENT, connection);
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            JobDetail job = newJob(Rabbit.class).build();
-            SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(period)
-                    .repeatForever();
-            Trigger trigger = newTrigger()
-                    .startNow()
-                    .withSchedule(times)
-                    .build();
-            scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
+            JobDetail job = getJobDetail();
+            scheduler.scheduleJob(job, getTrigger(getSimpleScheduleBuilder()));
+            Thread.sleep(10000);
+            scheduler.shutdown();
+        } catch (Exception se) {
             se.printStackTrace();
         }
     }
 
+    private Trigger getTrigger(SimpleScheduleBuilder times) {
+        return newTrigger()
+                .startNow()
+                .withSchedule(times)
+                .build();
+    }
+
+    private SimpleScheduleBuilder getSimpleScheduleBuilder() {
+        return simpleSchedule()
+                .withIntervalInSeconds(period)
+                .repeatForever();
+    }
+
+    private JobDetail getJobDetail() {
+        return newJob(Rabbit.class)
+                .usingJobData(data)
+                .build();
+    }
+
     public static class Rabbit implements Job {
+
+        public Rabbit() {
+            System.out.println(hashCode());
+        }
+
         @Override
         public void execute(JobExecutionContext context) {
-            System.out.println("Rabbit runs here ...");
+            try {
+                Connection connection = (Connection) context
+                        .getJobDetail()
+                        .getJobDataMap()
+                        .get(Keys.JOB_DATA_MAP_CONTENT);
+                try (PreparedStatement statement = connection
+                        .prepareStatement("insert into rabbit(created) values(?)")) {
+                    statement.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now()));
+                    statement.execute();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
+    }
+
+    private static class Keys {
+        static final String DRIVER = "driver-class-name";
+        static final String URL = "url";
+        static final String USER = "username";
+        static final String PASSWORD = "password";
+        final static String PERIOD = "rabbit.interval";
+        final static String JOB_DATA_MAP_CONTENT = "connection";
     }
 
     public static void main(String[] args) {
