ab080220ddba2b00ef20c0179b8aa649cae4cced
yor_email <daniil.stebnitsky@gmail.com>
2021-12-30 17:50:36 +0300

    Garbage Collector demonstration example.

---
+++ src/main/java/ru/job4j/chapter1/examples/gc/GCDemo.java
@@ -0,0 +1,27 @@
+package ru.job4j.chapter1.examples.gc;
+
+public class GCDemo {
+
+    private static final long KB = 1000;
+    private static final long MB = KB * KB;
+    private static final Runtime ENVIRONMENT = Runtime.getRuntime();
+
+    public static void info() {
+        final long freeMemory = ENVIRONMENT.freeMemory();
+        final long totalMemory = ENVIRONMENT.totalMemory();
+        final long maxMemory = ENVIRONMENT.maxMemory();
+        System.out.println("=== Environment state ===");
+        System.out.println(String.format("free: %d", freeMemory / MB));
+        System.out.println(String.format("total: %d", totalMemory / MB));
+        System.out.println(String.format("max: %d", maxMemory / MB));
+    }
+
+    public static void main(String[] args) {
+        info();
+        for (int i = 0; i < 10000; i++) {
+            new Person(i, "N" + i);
+        }
+        System.gc();
+        info();
+    }
+}
